048dec233cf48c265a78dd3874871e420ef182e2
Sanghyun Yun <545austin@naver.com>
2023-03-20 14:02:48 +0900

    quiz2

---
+++ c/program/checkprime.c
@@ -0,0 +1,7 @@
+int checkprmie (int n) {
+  for (i = 2; i <= n ; i++) {
+    if(i % 2 ==0)
+    return 0;
+  else
+    return 1;
+}
---
+++ c/program/qui2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include "libcheckprime.h"
+
+void main() {
+  int n;
+  printf("Input Number : ");
+  scanf("%d", &n);
+      printf("%d is a primenumber~!!")
+    else
+      printf("%d is not a primenumber~!!")
+  }
+}
