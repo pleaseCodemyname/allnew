630133a2ada071ae82e19a23afc4b204cc6663e2
Sanghyun Yun <545austin@naver.com>
2023-03-20 15:09:19 +0900

    checkprime.c_redo

--- c/program/checkprime.c
+++ c/program/checkprime.c
@@ -1,10 +1,10 @@
-int checkprmie (int n) {
+int checkprime(int n) {
   int i;
-  for (i = 2; i <= n ; i++) {
-    if(n%i ==0)
+  for (i=2; i <= n; i++) {
+    if (n%i == 0)
       break;
     else
       continue;
-    }
-    return i;
   }
+  return i;
+}
