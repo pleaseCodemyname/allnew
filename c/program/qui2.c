5b3f27c1f1943f0a63f4be6d2c39dcdf06b8dba3
Sanghyun Yun <545austin@naver.com>
2023-03-20 14:16:19 +0900

    quiz2_redo

--- c/program/qui2.c
+++ c/program/qui2.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
 #include "libcheckprime.h"
 
-void main() {
-  int n;
-  printf("Input Number : ");
-  scanf("%d", &n);
-      printf("%d is a primenumber~!!")
+void main()
+{
+  while (1)
+    int n;
+    printf("\ninput integer => ");
+    scanf("%d", &n);
+    if (n == 0) break;
+    if (checkprime(n) == n)
+      printf("%d is a prime number \n", n);
     else
-      printf("%d is not a primenumber~!!")
+      printf("%d is not a prime number \n", n);
   }
 }
