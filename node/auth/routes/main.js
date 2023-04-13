1fbf3979c34bae6c20fa53535970bbb401624002
pleaseCodemyname <545austin@naver.com>
2023-04-13 11:44:35 +0900

    register

--- node/auth/routes/main.js
+++ node/auth/routes/main.js
@@ -14,7 +14,7 @@ var connection = new mysql({
 const app = express()
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));0
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -42,10 +42,37 @@ app.post('/login', (req, res) => {
 // register
 app.post('/register', (req, res) => {
     const { id, pw } = req.body;
-    const result = connection.query("insert into user values (?, ?)", [id, pw]);
-    console.log(result);
-    res.redirect('/');
-})
+    if (id == "") {
+        res.redirect('register.html')
+    } else {
+        let result = connection.query("select * from user where userid=?", [id]);
+        if (result.length > 0) {
+            res.writeHead(200);
+            var template = `
+            <!doctype html>
+            <html>
+            <head>
+                <title>Error</title>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <div>
+                    <h3 style="margin-left: 30px">Register Failed</h3>
+                    <h4 style="margin-left: 30px">이미 존재하는 아이디입니다.</h4>
+                    <a href="register.html" style="margin-left: 30px"> 다시 시도하기</a>
+                </div>
+            </body>
+            </html>
+            `;
+            res.end(template);
+        } else {
+            result = connection.query("insert into user values (?, ?)",
+            [id, pw])/
+            console.log(result);
+            res.redirect('/');
+        }
+        }
+    })
 
 // request O, query X
 app.get('/select', (req, res) => {
